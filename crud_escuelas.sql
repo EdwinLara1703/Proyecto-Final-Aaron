a3de175376670de23eb2ffc5075f644556ca01b0
EDWIN AXEL LARA JAIMES <162504035+EdwinLara1703@users.noreply.github.com>
2024-11-26 16:28:01 -0600

    Add files via upload

---
+++ crud_escuelas.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE crud_escuelas;
+
+USE crud_escuelas;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    numero_cuenta VARCHAR(20) NOT NULL,
+    escuela ENUM('Fes Aragón', 'Fes Zaragoza', 'Fes Iztacala', 'Fes Cuautitlán', 'Fes Acatlán', 'CU') NOT NULL
+);
